e04edde59dce8a64b4944a7eb51dd2e1f30939d4
Yuya Ishikawa <ishikawa-yuya@functional-inc.com>
2025-08-31 16:46:50 +0900

    add: 定数ファイル作成

--- script/index.ts
+++ script/index.ts
@@ -5,25 +5,7 @@ import decompress from 'decompress'
 import encodingJapanese from 'encoding-japanese'
 import { parse as csvParse } from 'csv-parse/sync';
 import { stringify as csvStringify } from 'csv-stringify/sync';
-
-const CSV_HEADER_FIELDS = [
-  // [各項目の詳細]{@link https://www.post.japanpost.jp/zipcode/dl/readme.html}
-  'code', // 1.全国地方公共団体コード（JIS X0401、X0402）………　半角数字
-  'old_zip_code', //2.（旧）郵便番号（5桁）………………………………………　半角数字
-  'zip_code', // 3.郵便番号（7桁）………………………………………　半角数字
-  'prefecture_kana', // 4.都道府県名　…………　半角カタカナ（コード順に掲載）　（注1）
-  'city_kana', // 5.市区町村名　…………　半角カタカナ（コード順に掲載）　（注1）
-  'town_kana', // 6.町域名　………………　半角カタカナ（五十音順に掲載）　（注1）
-  'prefecture', // 7.都道府県名　…………　漢字（コード順に掲載）　（注1,2）
-  'city', // 8.市区町村名　…………　漢字（コード順に掲載）　（注1,2）
-  'town', // 9.町域名　………………　漢字（五十音順に掲載）　（注1,2）
-  'has_multiple_zip_codes', // 10.一町域が二以上の郵便番号で表される場合の表示　（注3）　（「1」は該当、「0」は該当せず）
-  'needs_koaza', // 11.小字毎に番地が起番されている町域の表示　（注4）　（「1」は該当、「0」は該当せず）
-  'has_chome', // 12.丁目を有する町域の場合の表示　（「1」は該当、「0」は該当せず）
-  'shares_zip_code', // 12.一つの郵便番号で二以上の町域を表す場合の表示　（注5）　（「1」は該当、「0」は該当せず）
-  'update_status', // 13.更新の表示（注6）（「0」は変更なし、「1」は変更あり、「2」廃止（廃止データのみ使用））
-  'change_reason', // 14.変更理由　（「0」は変更なし、「1」市政・区政・町政・分区・政令指定都市施行、「2」住居表示の実施、「3」区画整理、「4」郵便区調整等、「5」訂正、「6」廃止（廃止データのみ使用））
-] as const
+import { CSV_HEADER_FIELDS } from './src/const'
 
 type ZipCodeData = Record<typeof CSV_HEADER_FIELDS[number], string>
 
